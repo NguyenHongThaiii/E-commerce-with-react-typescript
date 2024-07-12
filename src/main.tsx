bf239476a3da5dda11551af8197fc2af3f7a5141
Thaipeii <tranvotam123@gmail.com>
2024-07-12 20:18:08 +0700

    fix path

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
 import './index.css'
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
